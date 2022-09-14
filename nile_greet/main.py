5fc1df6bdd264f725e6558453d72316680b58c96
Eric Lau <ericglau@outlook.com>
2022-09-14 17:16:22 -0400

    Remove alias

--- nile_greet/main.py
+++ nile_greet/main.py
@@ -5,17 +5,15 @@ from nile.nre import NileRuntimeEnvironment
 # Decorate the method that will be the command name with `click.command`
 @click.command()
 @click.argument("contract_name", type=str)
-@click.argument("contract_alias", type=str)
 # You can define custom parameters as defined in `click`: https://click.palletsprojects.com/en/7.x/options/
-def greet(contract_name, contract_alias):
+def greet(contract_name):
     # Help message to show with the command
     """
     Subcommand plugin that does something.
     """
     # Done! Now implement your custom functionality in the command
     click.echo(f"contract_name ${contract_name}")
-    click.echo(f"contract_alias ${contract_alias}")
     nre = NileRuntimeEnvironment()
 
-    address, abi = nre.deploy(contract_name, alias=contract_alias)
+    address, abi = nre.deploy(contract_name)
     print(abi, address)
